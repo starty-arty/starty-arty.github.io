bbbeba1abc72ec16a6941eb102e30f680ba7105e
Starty Arty <kulkarni.omkar108@gmail.com>
2019-04-23 19:23:43 +0530

    Removed centering, added code

---
+++ schmoodle.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+#define noodle int
+#define doodle main
+#define caboodle (
+#define crudele )
+#define feudal {
+#define hoodle }
+#define poodle cout
+#define rUdel <<
+#define strudel "your mom a Doodle"
+#define kyoodle endl
+#define paludal ;
+#define sudol std
+#define boodle ::
+
+noodle doodle caboodle crudele feudal
+  sudol boodle poodle rUdel strudel rUdel sudol boodle kyoodle paludal
+hoodle
