88b171148fb922cebece7b12dea4570581a45f64
Omkar Kulkarni <kulkarni.omkar108@gmail.com>
2024-12-26 17:55:43 +0530

    routing example

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from "./logo.svg";
-import "./App.css";
+// src/App.js
+
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useNavigate,
+  Outlet,
+} from "react-router-dom";
+
+// Home Page Component
+const Home = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <h2>Home Page</h2>
+      <button onClick={() => navigate("/contact")}>Go to Contact</button>
+    </div>
+  );
+};
+
+// About Page Component
+const About = () => (
+  <div>
+    <h2>About Page</h2>
+    <nav>
+      <ul>
+        <li>
+          <Link to="team">Our Team</Link>
+        </li>
+        <li>
+          <Link to="company">Our Company</Link>
+        </li>
+      </ul>
+    </nav>
+    <Outlet />
+  </div>
+);
+
+// Components for other pages
+const Contact = () => <h2>Contact Page</h2>;
+const Team = () => <h2>Team Page</h2>;
+const Company = () => <h2>Company Page</h2>;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and delete to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+      {/*Implementing Routes for respective Path */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />}>
+          <Route path="team" element={<Team />} />
+          <Route path="company" element={<Company />} />
+        </Route>
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
   );
 }
 
